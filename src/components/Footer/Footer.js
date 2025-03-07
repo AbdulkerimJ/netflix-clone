b9c0aff2cf9e82b033854d59e58f0d22140bb2c0
Abdulkerim <kerimewa@gmail.com>
2025-03-07 23:53:08 +0300

    add header and footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './Footer.css';
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+
+function Footer() {
+  return (
+    <div className="footer_outer_container">
+      <div className="footer_inner_container">
+        <div className="footer_icons">
+          <FacebookOutlinedIcon />
+          <InstagramIcon />
+          <YouTubeIcon />
+        </div>
+        <div className="footer_data">
+          <div>
+            <ul>
+              <li>Audio Description</li>
+              <li>Investor Relations</li>
+              <li>Legal Notice</li>
+              <li>&copy; 2025 Netflix. All rights reserved.</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Help Center</li>
+              <li>Jobs</li>
+              <li>Cookie Preferences</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Terms of Use</li>
+              <li>Privacy</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
